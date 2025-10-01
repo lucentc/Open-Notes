c3d9d64dcbd263e8ac3c6cb11c3d8d103efbc83b
lucentc <150416567+lucentc@users.noreply.github.com>
2025-10-02 00:17:35 +0700

    0.0.1.11

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -16,43 +17,4 @@ module.exports = {
     },
   },
   plugins: [],
-  safelist: [
-    "bg-white",
-    "dark:bg-neutral-100",
-    "bg-gray-200",
-    "dark:bg-gray-600",
-    "bg-black",
-    "dark:bg-neutral-800",
-    "text-white",
-    "bg-pink-200",
-    "dark:bg-pink-600",
-    "bg-red-200",
-    "dark:bg-red-600",
-    "bg-orange-200",
-    "dark:bg-orange-600",
-    "bg-yellow-200",
-    "dark:bg-yellow-600",
-    "bg-green-200",
-    "dark:bg-green-600",
-    "bg-emerald-200",
-    "dark:bg-emerald-600",
-    "bg-teal-200",
-    "dark:bg-teal-600",
-    "bg-sky-200",
-    "dark:bg-sky-600",
-    "bg-blue-200",
-    "dark:bg-blue-600",
-    "bg-indigo-200",
-    "dark:bg-indigo-600",
-    "bg-purple-200",
-    "dark:bg-purple-600",
-    "bg-violet-200",
-    "dark:bg-violet-600",
-    "bg-rose-200",
-    "dark:bg-rose-600",
-    "border-gray-200",
-    "dark:border-gray-700",
-    "ring-black/60",
-    "dark:ring-white/60"
-  ],
 }
